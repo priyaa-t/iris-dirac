11b77ffc9dc838e8c6893f26b42217dbec3585fa
Rohini Joshi <r.joshi@skatelescope.org>
2019-10-25 12:23:58 +0100

    Add CVMFS CASA install check to test payload

--- centos-home/test-payload/TestPayload.sh
+++ centos-home/test-payload/TestPayload.sh
@@ -24,3 +24,9 @@ echo "Check an image on cvmfs"
 echo "============="
 singularity exec --cleanenv /cvmfs/sw.skatelescope.eu/images/LOFARSoftwareSarrvesh.simg  wsclean --version
 echo "============="
+
+export PATH=/cvmfs/sw.skatelescope.eu/casa/casa-release-5.6.0-60.el7/bin:$PATH
+echo "Check cvmfs casa install"
+echo "============="
+casa --help
+echo "============="
